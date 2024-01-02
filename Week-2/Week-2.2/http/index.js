a7ee0561cbf152448330fab3deac49aa63fe0d92
MinDlessAK <MinDlessAK@users.noreply.github.com>
2024-01-02 13:15:59 +0530

    node

--- Week-2/Week-2.2/http/index.js
+++ Week-2/Week-2.2/http/index.js
@@ -22,4 +22,12 @@ app.listen(port,function(){
     console.log(`Welcome to My Server gandfatika${port}`)
 })
 
+// When to Use Each
+
+// GET: Use when you want to retrieve data from the server, and the request has no side effects. It's suitable for safe and idempotent operations.
+// POST: Use when you want to submit data to the server, especially when the operation has side effects (e.g., creating a new resource on the server). It's suitable for non-idempotent operations.
+
+// In summary, the choice between GET and POST depends on the purpose of the request and the type of operation you are performing.
+
+
 //go through later (pending)
