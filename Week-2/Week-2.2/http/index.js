2864587dc3bc092cf447395fa8d7d20601a45e0a
MinDlessAK <MinDlessAK@users.noreply.github.com>
2024-01-03 02:21:06 +0530

    week-2.5

--- Week-2/Week-2.2/http/index.js
+++ Week-2/Week-2.2/http/index.js
@@ -6,16 +6,14 @@ const port=6500
 app.use(bodyParser.json())
 
 // app.get('/',function(req,res){
-//     res.send("Hello From gand fattika server")
+       // console.log(req.headers)
+      // console.log(req.headers.authorization)
+      // console.log(req.body)
 // })
 
 app.post('/',function(req,res){
-    console.log(req.headers)
-    console.log(req.headers.authorization)
-    console.log(req.body)
     res.send("Hello From gand fattika server")
 })
-
 
 
 app.listen(port,function(){
