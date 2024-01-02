32516a9f6115abf8d314bf90bbd150bc4915ecf6
MinDlessAK <MinDlessAK@users.noreply.github.com>
2024-01-03 02:20:40 +0530

    week-2.5

---
+++ Week-2/Week-2.5/code/index.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const bodyParser=require('body-parser')
+
+const app = express();
+
+
+app.use(bodyParser.json())
+
+const users=[{
+    name:"akash",
+    kidney:[{
+        healthy:false
+    }]
+}]
+
+app.get('/', function (req, res) {
+   let akashkidneys= users[0].kidney
+   let nameOfKidneyContractor=users[0].name
+   let numberOfkidney=akashkidneys.length
+   let numberOfHealthykidney=0
+      for(let i=0;i<akashkidneys.length;i++){
+        if(akashkidneys[i].healthy){
+          numberOfHealthykidney=numberOfHealthykidney+1
+        }
+      }
+     let numberOfUnHealthykidney=numberOfkidney-numberOfHealthykidney;
+    
+     res.json({
+        nameOfKidneyContractor,
+        numberOfkidney,
+        numberOfHealthykidney,
+        numberOfUnHealthykidney
+     }
+     )
+})
+
+
+app.post('/',function(req,res){
+    const isHealthy=req.body.isHealthy
+
+    users[0].kidney.push({
+        healthy: isHealthy
+    })
+
+    res.json({
+        "msg":"Done"
+    })
+})
+
+app.put('/',function(req,res){
+
+    for(let i=0;i<users[0].kidney.length;i++){
+           users[0].kidney[i].healthy=true
+      }
+      
+     res.json({
+        "msg":"done"
+     })
+})
+
+
+app.delete('/',function(req,res){
+    let newKidney=[]
+    for(let i=0;i<users[0].kidney.length;i++){
+       if(users[0].kidney[i].healthy){
+          newKidney.push({
+            healthy:true
+          })
+       }
+   }
+   users[0].kidney=newKidney
+   
+  res.json({
+     "msg":"done"
+  })
+})
+
+
+
+app.listen(2929)
