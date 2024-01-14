9551e501da5b3ae2476723f26e4d0909a34b8988
Akash Mishra <122022486+MinDlessAK@users.noreply.github.com>
2024-01-14 23:59:26 +0530

    Week-4

--- Week-4/Week-4.3/mongoose/index.js
+++ Week-4/Week-4.3/mongoose/index.js
@@ -1,1 +1,28 @@
 const mongoose = require('mongoose')
+const express =require('express')
+
+const app=express()
+
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://akash007:rUALKxScojWSwAc2@cluster0.caaivkb.mongodb.net/")
+
+const userSchema=new mongoose.Schema({
+    username:String,
+    password:String
+})
+
+const User=mongoose.model('Users',userSchema)
+
+
+app.get('/',(req,res)=>{
+    username=req.body.username
+    password=req.body.password
+    User.create({username,password})
+   res.json({username,password})
+})
+
+
+app.listen(3000)
+
+
