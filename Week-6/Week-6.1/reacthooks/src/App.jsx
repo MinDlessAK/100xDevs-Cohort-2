97ed8637e1e17686611e4ceb46aa72ae071a60c0
Akash Mishra <122022486+MinDlessAK@users.noreply.github.com>
2024-01-27 23:44:27 +0530

    Week

--- Week-6/Week-6.1/reacthooks/src/App.jsx
+++ Week-6/Week-6.1/reacthooks/src/App.jsx
@@ -1,54 +1,137 @@
-import { useState } from "react"
+import { useState} from "react"
+// import React from "react"
+// import Props from "./Props"
+
+// let counter=4;
 
 function App() {
   // const [change,setchange]=useState('harkirat')
-    const [title,setTitle]=useState(0)
+    // const [title,setTitle]=useState("my name is 0")
 
-  function ak(){
-    setTitle("my name is" + Math.random())
-  }
+  // function ak(){
+  //   setTitle("my name is " + Math.random())
+  // }
   
+  //todo
+  // const[todos,setTodo]=useState([{
+  //   id:1,
+  //   title:"gym",
+  //   description:"Go to gym today"
+  //  },{
+  //   id:2,
+  //   title:"food",
+  //   description:"eat buger today"
+  //  },{
+  //   id:3,
+  //   title:"class",
+  //   description:"attend class at 12pm"
+  //  }])
+  
+    // function addtodo(){
+    //   setTodo([...todos,{
+    //     id:counter++,
+    //     title:title,
+    //     description:description
+    //   }])
+    // }
+   
+
+
+    // const [title,setTitle]=useState("")
+    // const [description,setDescription]=useState("")
+    
+
  return (
     <div>
-         {/* <Update/> */}
-        <button onClick={ak}>change</button> 
+        <Update /> 
+         {/* <button onClick={()=>{setchange('priyanshu')}}>change</button> 
+        <Props change={change} /> */}
+
+        {/* <button onClick={ak}>change</button>
+        <Header title={title} ></Header>
         <Header title={title} ></Header>
         <Header title="chupereactwalle " ></Header>
         <Header title="chupereactwalle " ></Header>
-        <Header title="chupereactwalle " ></Header>
+        <Header title="chupereactwalle " ></Header> */}
+
         {/* <button onClick={()=>{setchange(Math.random())}}>change</button>
         <h1>my name is{change}</h1>
         <h1>kdkndcskln</h1> */}
-    </div>
+          {/* Todo */}
+    
+
+    {/* <input type="text" onChange={(e)=>{setTitle(e.target.value)}} /><br/>
+    <input  type="text" onChange={(e)=>{setDescription(e.target.value)}} /><br/><br/>
+    <button onClick={addtodo}>Add a todo</button><br/> */}
+        {/* {todos.map(function(todo) {
+          return(
+           <Todo key={todo.id} title={todo.title} description={todo.description} />
+           )
+           })} */} 
+           {/* {todos.map(todo=><Todo key={todo.id} title={todo.title} description={todo.description}/>)} */}
+
+        {/* <Wrapper innercomponent={<TextComponent/>} />
+        <CardWrapper>
+        <TextComponent></TextComponent>
+        </CardWrapper> */}
+
+   </div>
   )
 }
 
-// function Update(){
-//   const [title,setTitle]=useState(0)
+function Update(){
+  const [title,setTitle]=useState(0)
 
-//   function ak(){
-//     setTitle(Math.random())
-//   }
-//   return(
-//     <>
-//      <button onClick={ak}>change</button>
-//      <h1>My name is {title}</h1>
-//     </>
-//   )
+  function ak(){
+    setTitle(Math.random())
+  }
+  return(
+    <>
+     <button onClick={ak}>change</button>
+     <h1>My name is {title}</h1>
+    </>
+  )
 
+}
+
+
+// const Header = React.memo(function Header({title}) {
+//   return (
+//      <div>
+//          {title}
+//      </div>
+//    )
+//  })
+
+// function Todo({title,description}){
+//     return(
+//       <div>
+//         <h1>{title}</h1>
+//         <h2>{description}</h2>
+//       </div>
+//     )
+// }
+// function TextComponent(){
+//   return<div>
+//     hi there!
+//   </div>
 // }
 
+// function Wrapper({innercomponent}){
+//   return<>
+//      <div style={{border:"2px solid black"}}>
+//        {innercomponent}
+//      </div>
+//   </>
+// }
 
-function Header({title}) {
-  return (
-     <div>
-         {title}
-     </div>
-   )
- }
-
-
- 
+// function CardWrapper({children}){
+//      return<>
+//         <div style={{border:"2px solid black"}}>
+//           {children}
+//         </div>
+//      </>
+// }
 
 
 export default App
---
+++ Week-6/Week-6.2/reacthook/src/App.jsx
@@ -0,0 +1,124 @@
+// import { useState } from "react";
+// import { useEffect } from "react";
+// import axios from 'axios'
+
+// function App() {
+//   const [inc,setInc]=useState(0);
+//   const [sum ,setSum]=useState(0)
+
+//   let count=0
+
+//   for(let i=0;i<=sum;i++){
+//     count=count+i
+//   }
+
+//   return <div>
+  
+//     <Todo id={2} />
+
+//     {/* sum */}
+//     <div>
+//      <input type="text" onChange={(e)=>{setSum(e.target.value)}}></input>
+//       <button onClick={()=>setInc(inc+1)}>Counter {inc}</button>
+//       <h2>Sum of {sum} is {count}</h2>
+//     </div>
+
+//   </div>
+// }
+
+
+
+
+// function Todo({id}) {
+//   const [todo, setTodo] = useState({});
+//  const [od,setod]=useState(id)
+
+//   useEffect(() => {
+//     axios.get("https://sum-server.100xdevs.com/todo?id=" + od)
+//       .then(async function(res) {
+//         // const json = await res.json();
+//         setTodo (res.data.todo);
+//       })
+//   }, [od])
+
+//   return <div>
+//    <button onClick={(e)=>{if(e.target.innerText==1){setod(e.target.innerText)}}}>1</button>
+//    <button onClick={(e)=>{if(e.target.innerText==2){setod(e.target.innerText)}}}>2</button>
+//    <button onClick={(e)=>{if(e.target.innerText==3){setod(e.target.innerText)}}}>3</button>
+//    <button onClick={(e)=>{if(e.target.innerText==4){setod(e.target.innerText)}}}>4</button>
+//     <h1>
+//       {todo.title}
+//     </h1>
+//     <h4>
+//       {todo.description}
+//     </h4>
+//   </div>
+// }
+
+// export default App;
+
+
+/*
+import  { useState, useMemo } from 'react';
+
+const ExpensiveCalculation = ({ value }) => {
+  const expensiveResult = useMemo(() => {
+    // Simulating a computationally expensive operation
+    console.log('Calculating expensive result...');
+    return value * 2;
+  }, [value]); // Dependency array: recalculates when 'value' changes
+
+  return (
+    <div>
+      <p>Value: {value}</p>
+      <p>Expensive Result: {expensiveResult}</p>
+    </div>
+  );
+};
+
+const MemoExample = () => {
+  const [inputValue, setInputValue] = useState(5);
+
+  return (
+    <div>
+      <input
+        type="number"
+        value={inputValue}
+        onChange={(e) => setInputValue(Number(e.target.value))}
+      />
+      <ExpensiveCalculation value={inputValue} />
+    </div>
+  );
+};
+
+export default MemoExample;
+*/
+
+import React, { useState, useCallback } from 'react';
+
+const ChildComponent = ({ onClick }) => {
+  console.log('ChildComponent rendering...');
+  return <button onClick={onClick}>Click me</button>;
+};
+
+const CallbackExample = () => {
+  const [count, setCount] = useState(0);
+
+  // Regular callback function
+  const handleClick = () => {
+    console.log('Button clicked!');
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  // Memoized callback using useCallback
+  const memoizedHandleClick = useCallback(handleClick, []);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <ChildComponent onClick={memoizedHandleClick} />
+    </div>
+  );
+};
+
+export default CallbackExample;
